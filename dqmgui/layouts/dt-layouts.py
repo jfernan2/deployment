86a7fea0f16d63a73ed0196e4962ba7374457afb
Javier <Javier.Fernandez@cern.ch>
2016-07-15 17:32:33 +0200

    Fixed index in sector number

--- dqmgui/layouts/dt-layouts.py
+++ dqmgui/layouts/dt-layouts.py
@@ -135,7 +135,7 @@ for wheel in range(-2, 3):
 #### QUALITY vs PHI RAD ############################################################################
 for wheel in range(-2, 3):
     for station in range (1, 5):
-        for sector in range (1, 12):
+        for sector in range (1, 13):
             name = "11-QualityPhi/Wheel" + str(wheel) + "/Sector" + str(sector) + "_Station" + str(station)
             histoname = "DT/03-LocalTrigger-TM/Wheel" + str(wheel)+ "/Sector" + str(sector) + "/Station" + str(station) + "/LocalTriggerPhi/TM_QualvsPhirad_W" + str(wheel) + "_Sec" + str(sector) + "_St" + str(station)
 
